bbf7327cfd5dabfbbdc56cd3264e7cbb42ac1ea5
mohanperiyasami <102672742+mohanperiyasami@users.noreply.github.com>
2022-10-18 13:29:09 +0530

    Delete Bank.java

--- TNS FOUNDATION/src/Bank.java
+++
@@ -1,7 +0,0 @@
-
-abstract class Bank{	
-	public abstract void Account(String Number,double Balance,String Name,String Email,String PhoneNumber);	
-	public abstract void DepositMoney(double DepositedMoney);
-	public abstract void WithDrawMoney(double WithdrawalMoney);
-	
-}
