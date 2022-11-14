3864d38c02a730099cf97a97302a81da2d5f6dbf
mohanperiyasami <102672742+mohanperiyasami@users.noreply.github.com>
2022-11-14 18:25:38 +0530

    Update code01_access_package1_package_dot_asterisk.java

--- encapsulation/code01_access_package1_package_dot_asterisk.java
+++ encapsulation/code01_access_package1_package_dot_asterisk.java
@@ -1,9 +1,4 @@
-// A java package is a group of similar types of 
-// classes, interfaces and sub-packages.
 
-// ------------------- FILE 1 --------------------
-
-//save by A.java  
 package pack;  
 public class A{  
   public void msg(){System.out.println("Hello");}  
